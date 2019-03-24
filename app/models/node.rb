3e31b968bb8821a30be90bb01e9fa97ac93023a1
Pierre Abreu <pierre@edumobi.com.br>
2019-03-24 11:15:30 -0300

    Model: add Node parent relation

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,4 +1,5 @@
 class Node < ApplicationRecord
   belongs_to :tree
+  belongs_to :parent, foreign_key: :parent_id, class_name: 'Node', optional: true
   has_many :children, foreign_key: :parent_id, class_name: 'Node'
 end
